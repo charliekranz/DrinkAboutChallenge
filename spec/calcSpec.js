1a7b20db3801545c531f27b9cf485b0a7e518224
charliekranz <charliekranz3@gmail.com>
2020-11-30 12:34:37 +0000

    Chanes to calcSpec

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,5 +1,7 @@
 describe("whatCanIDrink". function(){
     describe("age", function () {
-
-    })
-})
+        it(""whatIsAppropriateToDrink", function () {
+            expect(age(0)).toBe("whatIsAppropriateToDrink"));
+        });
+    });
+});
